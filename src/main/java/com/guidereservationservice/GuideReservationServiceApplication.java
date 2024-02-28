6a23a5a78ced75a56039257ddf465f873b3b0a5e
ismail telhouni <ismailtelhouni123@gmail.com>
2024-02-28 05:54:16 +0100

    first commit

---
+++ src/main/java/com/guidereservationservice/GuideReservationServiceApplication.java
@@ -0,0 +1,13 @@
+package com.guidereservationservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GuideReservationServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GuideReservationServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/guidereservationservice/GuideReservationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.guidereservationservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GuideReservationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
